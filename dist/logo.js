f12659c3d90afaf00ced302e16dfa491876a4487
Yield Alt <yieldofmemes@protonmail.com>
2021-02-12 22:21:30 -0500

    remove scripts

--- dist/logo.js
+++ dist/logo.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-if( $('.inside h1').length ){
-  var characters = $('h1').text().split("");
-
-  $this = $('h1');
-  $this.empty();
-  $.each(characters, function (i, el) {
-      $this.append("<span>" + el + "</span");
-  });
-}
-
-$('p, span, .values a').html(function(i, v){
-    return v.replace(/(\d)/g, '<span class="number">$1</span>');
-});
+// if( $('.inside h1').length ){
+//   var characters = $('h1').text().split("");
+//
+//   $this = $('h1');
+//   $this.empty();
+//   $.each(characters, function (i, el) {
+//       $this.append("<span>" + el + "</span");
+//   });
+// }
+//
+// $('p, span, .values a').html(function(i, v){
+//     return v.replace(/(\d)/g, '<span class="number">$1</span>');
+// });
 
 
 
